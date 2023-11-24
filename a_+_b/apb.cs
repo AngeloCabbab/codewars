7a015edfacafc5d206fa06c4a27ecda46146a636
AngeloCabbab <122501767+AngeloCabbab@users.noreply.github.com>
2023-11-24 14:02:03 +0800

    Add files via upload

---
+++ a_+_b/apb.cs
@@ -0,0 +1,11 @@
+// [a + b]
+// [8 kyu]
+// [c#]
+public class FirstClass 
+{
+public static int sum(int a, int b) 
+{
+int c = (a + b);
+return c;
+}
+}
---
+++ a_+_b/apb.java
@@ -0,0 +1,11 @@
+// [a + b]
+// [8 kyu]
+// [java]
+public class FirstClass
+{
+public static int sum(int a, int b)
+{
+int c = (a + b);
+return c;
+}
+}
